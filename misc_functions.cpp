309bfea55af95d6a1222c8ac170caec9c25b35bc
KeyFicller <whuzyh1998@163.com>
2026-01-15 21:48:05 +0800

    remove less used file.

--- misc_functions.cpp
+++
@@ -1,9 +0,0 @@
-#include "misc_functions.h"
-
-#include <iostream>
-
-void query_maximum_vertex_attributes() {
-  GLint max_vertex_attribs;
-  glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &max_vertex_attribs);
-  std::cout << "Maximum vertex attributes: " << max_vertex_attribs << std::endl;
-}
--- misc_functions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <glad/gl.h>
-
-#include <GLFW/glfw3.h>
-
-void query_maximum_vertex_attributes();
